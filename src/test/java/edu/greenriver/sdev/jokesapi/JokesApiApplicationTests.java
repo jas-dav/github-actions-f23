212b72886993624bee937241324b303f02b89cec
Jasmine David <jas-dav@users.noreply.github.com>
2023-12-06 06:52:02 -0800

    Adding a small change to my tests

--- src/test/java/edu/greenriver/sdev/jokesapi/JokesApiApplicationTests.java
+++ src/test/java/edu/greenriver/sdev/jokesapi/JokesApiApplicationTests.java
@@ -25,6 +25,7 @@ class JokesApiApplicationTests
     @Test
     public void contextLoads()
     {
+        System.out.println("Spring app loaded...");
 
     }
 
@@ -44,6 +45,7 @@ class JokesApiApplicationTests
         HttpStatusCode status = response.getStatusCode();
         Joke[] jokes = response.getBody();
 
+        System.out.println("Assert our results");
         assertEquals(status, HttpStatus.OK);
         assertTrue(jokes.length > 0);
     }
